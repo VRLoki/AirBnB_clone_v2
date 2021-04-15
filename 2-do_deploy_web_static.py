0e5d118dee4e78729b672c3acbfb957deaa08d3a
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-04-15 10:42:00 +0000

    Fix 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,31 +9,24 @@ env.hosts = ['35.229.40.200', '35.229.23.118']
 
 
 def do_deploy(archive_path):
-    """ Function that distributes the archive.
-
-    Args:
-        archive_path (str): the path of the archive to deploy on the servers.
-    """
+    """Deploys our website to our servers!"""
 
     if not path.exists(archive_path):
         return False
 
-    name = archive_path.split("/")[-1]
-    name_noext = name.split(".")[0]
-
-    remote = "/data/web_static/releases/"
+    filename = archive_path.split("/")[-1]
+    noext, ext = path.splitext(filename)
+    remotepath = "/data/web_static/releases/"
 
     try:
         put(archive_path, '/tmp')
-        run('mkdir -p {}{}/'.format(remote, name_noext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(name, remote, name_noext))
-        run('rm -f /tmp/{}'.format(name))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(remote, name_noext))
-        run('rm -rf {}{}/web_static'.format(remote, name_noext))
+        run('mkdir -p {}{}/'.format(remotepath, noext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(filename, remotepath, noext))
+        run('rm -f /tmp/{}'.format(filename))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(remotepath, noext))
+        run('rm -rf {}{}/web_static'.format(remotepath, noext))
         run('rm -rf /data/web_static/current')
-        run('ln -sf {}{}/ /data/web_static/current'.format(remote, name_noext))
+        run('ln -sf {}{}/ /data/web_static/current'.format(remotepath, noext))
         print("New version deployed!")
-        return True
-
     except:
         return False

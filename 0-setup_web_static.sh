86fe20635033ba074e203220d6422bd6a8cde0bd
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-04-15 09:55:37 +0000

    Fix 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,8 +2,8 @@
 # Sets up the web servers for the deployment of web_static.
 
 # Install Nginx if not already installed
-apt update -y >/dev/null 2>&1
-apt install nginx -y >/dev/null 2>&1
+apt update -y
+apt install nginx -y
 
 # Create required directories if they don't exist
 mkdir -p /data/web_static/
@@ -15,15 +15,13 @@ mkdir -p /data/web_static/releases/test/
 echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" >/data/web_static/releases/test/index.html
 
 # Create the symbolic link
-ln -snf /data/web_static/releases/test /data/web_static/current
+ln -sf /data/web_static/releases/test /data/web_static/current
 
 # Change ownership of files and folders inside of /data folder
-chown -hR ubuntu:ubuntu /data
+chown -R ubuntu:ubuntu /data
 
 # Add alias to serve the content of /data/web_static/current to hbnb_static
-if ! grep -q 'location \/hbnb_static\/' /etc/nginx/sites-available/default; then
-	sed -i '/server_name _;/a\\n        location /hbnb_static/ {\n                alias /data/web_static/current/;\n        }' /etc/nginx/sites-available/default
-fi
+sed -i '/server_name _;/a\\n        location /hbnb_static/ {\n                alias /data/web_static/current/;\n        }' /etc/nginx/sites-available/default
 
 # Restart the nginx service
 service nginx restart

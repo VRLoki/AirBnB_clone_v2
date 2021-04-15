0df5c612498344e5f6ff235b379069ace8833328
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-04-15 09:52:53 +0000

    Fix 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -18,7 +18,7 @@ echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\
 ln -snf /data/web_static/releases/test /data/web_static/current
 
 # Change ownership of files and folders inside of /data folder
-chown -R ubuntu:ubuntu /data
+chown -hR ubuntu:ubuntu /data
 
 # Add alias to serve the content of /data/web_static/current to hbnb_static
 if ! grep -q 'location \/hbnb_static\/' /etc/nginx/sites-available/default; then
